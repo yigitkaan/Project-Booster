3b4ae1c187b25d2897fc1fd81bd57fb2b6b57ad6
yigitkaan <65395094+yigitkaan@users.noreply.github.com>
2023-03-30 13:44:22 +0300

    QuitApplication script dosyası eklendi

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Debug.Log("Oyundan çıkıldı.");
+            Application.Quit();
+        }
+    }
+}
