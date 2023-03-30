3f80d394a271ca1d8c7af6b185f709f98b68d75e
yigitkaan <65395094+yigitkaan@users.noreply.github.com>
2023-03-30 12:59:26 +0300

    Level 2 tasarlandı.

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -18,12 +18,17 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) {//0 a bölünme hatasını engellemek için. Period 0 olursa nesne durur.
+            return;
+        }
+
         float cycles = Time.time / period; //Time.time nekadar zaman geçtiğini verir. Burda devamlı zaman büyür.
 
         const float tau = Mathf.PI * 2; // değişmez değer 6.283'ün.
         float rawSinWave = Mathf.Sin(cycles * tau);// -1 den 1 e gider.
 
         movementFactor = (rawSinWave + 1f) / 2f;// 0 dan 1 e giderken tekrardan hesaplanır.
+
         Vector3 offset = movementVector * movementFactor;
         gameObject.transform.position = startingPosition + offset;
     }
